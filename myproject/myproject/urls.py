27e038fa0aaec180c70c8c6e2f5802055686534d
O'Donnell Tomagan <dnnlltmgn1@gmail.com>
2025-04-18 13:00:46 +0800

    Linked urls from auth to projects

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('auth/', include('auth.urls'))
+    path('api/auth/', include('authentication.urls'))
 ]
